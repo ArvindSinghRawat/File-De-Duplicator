87f238e029382bc8634a97d5092cb46a45ffd80c
Arvind Singh Rawat <arvind160102261@gmail.com>
2024-12-07 16:01:41 +0000

    Boilerplate setup

---
+++ src/main/java/com/asr/example/file/deduplicator/FileDeDuplicatorApplication.java
@@ -0,0 +1,13 @@
+package com.asr.example.file.deduplicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDeDuplicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDeDuplicatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asr/example/file/deduplicator/FileDeDuplicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asr.example.file.deduplicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDeDuplicatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
