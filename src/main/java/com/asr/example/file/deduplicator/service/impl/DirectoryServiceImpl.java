2c8d3413a5c58f15113d81752b764bebe56b08cc
Arvind Singh Rawat <arvind160102261@gmail.com>
2024-12-08 20:36:09 +0530

    Added logic to save the Directory in database
    Signed-off-by: Arvind Singh Rawat <arvind160102261@gmail.com>

--- src/main/java/com/asr/example/file/deduplicator/service/impl/DirectoryServiceImpl.java
+++ src/main/java/com/asr/example/file/deduplicator/service/impl/DirectoryServiceImpl.java
@@ -1,20 +1,27 @@
 package com.asr.example.file.deduplicator.service.impl;
 
 import com.asr.example.file.deduplicator.dto.request.DirectoryRequest;
+import com.asr.example.file.deduplicator.entity.DirectoryEntity;
+import com.asr.example.file.deduplicator.repository.DirectoryRepository;
 import com.asr.example.file.deduplicator.service.DirectoryService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class DirectoryServiceImpl implements DirectoryService {
+
+    DirectoryRepository directoryRepository;
 
     private static Optional<String> validatePath(DirectoryRequest request) {
         Path sourcePath = Path.of(request.getSourcePath().trim());
@@ -31,6 +38,19 @@ public class DirectoryServiceImpl implements DirectoryService {
 
     @Override
     public Optional<String> savePath(DirectoryRequest request) {
-        return validatePath(request);
+        // Validate path
+        Optional<String> validationError = validatePath(request);
+        if (validationError.isPresent()) {
+            return validationError;
+        }
+        // Save path to database
+        try {
+            directoryRepository.save(DirectoryEntity.builder().sourceDirectory(request.getSourcePath()).build());
+        } catch (DataAccessException ex) {
+            log.warn("Exception while saving directory {} in the database, Error: {}",
+                    request.getSourcePath(), ex.getMessage(), ex);
+            return Optional.of(String.format("Exception while saving directory %s to database, please try again!", request.getSourcePath()));
+        }
+        return Optional.empty();
     }
 }
