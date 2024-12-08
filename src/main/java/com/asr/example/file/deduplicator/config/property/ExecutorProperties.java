05e96f15dcc0a7dc366b063d755355689c73a868
Arvind Singh Rawat <arvind160102261@gmail.com>
2024-12-08 21:21:34 +0530

    #1: Executor pool added with configuration
    Signed-off-by: Arvind Singh Rawat <arvind160102261@gmail.com>

---
+++ src/main/java/com/asr/example/file/deduplicator/config/property/ExecutorProperties.java
@@ -0,0 +1,25 @@
+package com.asr.example.file.deduplicator.config.property;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@ConfigurationProperties(prefix = "spring.app.async")
+public class ExecutorProperties {
+    int corePoolSize;
+    int maxPoolSize;
+    int queueCapacity;
+    String threadNamePrefix;
+
+    @ConstructorBinding
+    public ExecutorProperties(int corePoolSize, int maxPoolSize, int queueCapacity, String threadNamePrefix) {
+        this.corePoolSize = corePoolSize;
+        this.maxPoolSize = maxPoolSize;
+        this.queueCapacity = queueCapacity;
+        this.threadNamePrefix = threadNamePrefix;
+    }
+}
